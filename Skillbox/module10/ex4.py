828809f0a1d6f7eee713f144d47045ef4937a786
Max9874 <vyazovskymaxim7777@gmail.com>
2023-04-04 23:22:15 +0200

    hh

--- Skillbox/module10/ex4.py
+++ Skillbox/module10/ex4.py
@@ -2,10 +2,18 @@ amount_of_numbers = int(input("enter amount of numbers: "))
 divider = 2
 counter = 0
 
+
+def is_prime(number):
+    local_divider = 2
+    while number % local_divider != 0:
+        local_divider += 1
+    return local_divider == number
+
+
 for _ in range(amount_of_numbers):
-    number = int(input("Enter number: "))
-    while divider <= number:
-        if number % divider == 0 and number == divider or number == 1:
+    checking_number = int(input("Enter number: "))
+    while divider <= checking_number:
+        if checking_number % divider == 0 and checking_number == divider or checking_number == 1:
             counter += 1
         divider += 1
 
